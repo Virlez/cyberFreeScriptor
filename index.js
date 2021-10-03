20036ad638ca07ec6f355d4b599b9596feeaf28e
Pauline Ivaldi <pauline_ivaldi@hotmail.fr>
2021-10-03 19:49:44 +0200

    back to whole object

--- index.js
+++ index.js
@@ -344,8 +344,8 @@ async function transcribe_witai(file) {
         //if (output && '_text' in output && output._text.length)
             //return output._text
        // if (output && 'text' in output && output.text.length)
-            return output.text
-        //return output;
+            //return output.text
+        return output;
     } catch (e) { console.log('transcribe_witai 851:' + e) }
 }
 
