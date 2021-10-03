6b438a5da83cb3ec4acde951f115a7fadc57fdc9
Pauline Ivaldi <pauline_ivaldi@hotmail.fr>
2021-10-03 20:22:45 +0200

    add ToString()

--- index.js
+++ index.js
@@ -345,7 +345,7 @@ async function transcribe_witai(file) {
             //return output._text
         //if (output && 'text' in output && output.text.length)
             
-        return output['traits'];
+        return output['text'].toString();
     } catch (e) { console.log('transcribe_witai 851:' + e) }
 }
 
