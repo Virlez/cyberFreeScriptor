e4d3862af065b220696b182dc33251fcf497b7a7
Pauline Ivaldi <pauline_ivaldi@hotmail.fr>
2021-10-03 19:47:21 +0200

    change output.text

--- index.js
+++ index.js
@@ -342,9 +342,9 @@ async function transcribe_witai(file) {
         console.log(output)
         stream.destroy()
         //if (output && '_text' in output && output._text.length)
-            return output._text
+            //return output._text
        // if (output && 'text' in output && output.text.length)
-            //return output.text
+            return output.text
         //return output;
     } catch (e) { console.log('transcribe_witai 851:' + e) }
 }
