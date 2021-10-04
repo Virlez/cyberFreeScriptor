3e01305823caa91a2582bc1933b7b597248be560
Pauline Ivaldi <pauline_ivaldi@hotmail.fr>
2021-10-04 09:15:07 +0200

    delete .map

--- index.js
+++ index.js
@@ -340,7 +340,7 @@ async function transcribe_witai(file) {
         const output = await extractSpeechIntent(witAPIKEY, stream, "audio/wav")
         witAI_lastcallTS = Math.floor(new Date());
         console.log(output)
-        const textOut = output.map(x => {
+        const textOut = output(x => {
             return {
                 text : x.data.text};
             });
