c9dcf42ed902ac59dedb71a697aeb0578866a476
Pauline Ivaldi <pauline_ivaldi@hotmail.fr>
2021-10-06 13:19:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -352,8 +352,8 @@ async function transcribe_witai(file) {
                 text : x.data.text};
             });*/
             stream.destroy()
-            console.log(textJson[0]["text"])
-        return textJson[0]["text"];
+            console.log(textJson[0])
+        return textJson[0];
         
         //if (output && '_text' in output && output._text.length)
             //return output._text
