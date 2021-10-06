cc8ba196a1811dd987254068b71e81204f10e7be
Pauline Ivaldi <pauline_ivaldi@hotmail.fr>
2021-10-06 11:21:45 +0200

    change textout

--- index.js
+++ index.js
@@ -340,7 +340,7 @@ async function transcribe_witai(file) {
         const output = await extractSpeechIntent(witAPIKEY, stream, "audio/wav")
         witAI_lastcallTS = Math.floor(new Date());
         console.log(output)
-        const textOut = JSON.parse(output);
+        const textOut = JSON.parse(output)
         /*const justText = output(x => {
             return {
                 text : x.data.text};
@@ -353,7 +353,7 @@ async function transcribe_witai(file) {
         //if (output && '_text' in output && output._text.length)
             //return output._text
         //if (output && 'text' in output && output.text.length)
-         console.log(textOut.text)   
+         console.log(textOut)   
         return textOut.text;
     } catch (e) { console.log('transcribe_witai 851:' + e) }
 }
